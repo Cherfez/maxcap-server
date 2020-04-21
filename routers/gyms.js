2c57aaaa33ffa0656c4b8d97ad99919a4933863f
Cherfez <chervasev.leeuwen@gmail.com>
2020-04-21 16:58:06 +0200

    setup route for specific gym based on id

--- routers/gyms.js
+++ routers/gyms.js
@@ -9,4 +9,24 @@ router.get("/", async (req, res, next) => {
   res.status(200).send(gyms);
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).send({ message: "Gym id is not a number" });
+    }
+
+    const gym = await Gyms.findByPk(id);
+
+    if (gym === null) {
+      return res.status(404).send({ message: "Gym not found" });
+    }
+
+    res.status(200).send({ message: "ok", gym });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
