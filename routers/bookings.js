6fc46f13fa70035493e22314623fb897b5e688d2
Cherfez <chervasev.leeuwen@gmail.com>
2020-04-23 14:47:41 +0200

    Post route for bookings

--- routers/bookings.js
+++ routers/bookings.js
@@ -9,4 +9,32 @@ router.get("/", async (req, res, next) => {
   res.status(200).send(bookings);
 });
 
+router.post("/", auth, async (req, res, next) => {
+  try {
+    if (req.user.id === null) {
+      return res.status(400).send({ message: "Not logged in!" });
+    }
+    const { finalPartners, timeslotId, gymId } = req.body;
+
+    const namePartner = finalPartners;
+    // if (!comment) {
+    //   return res.status(400).send({ message: "Invalid comment" });
+    // }
+    // if (!serviceId) {
+    //   return res.status(400).send({ message: "Service not found" });
+    // }
+
+    const booking = await Bookings.create({
+      namePartner,
+      timeslotId,
+      gymId,
+      userId: req.user.id,
+    });
+
+    return res.status(201).send({ message: "Feedback added", feedback });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
